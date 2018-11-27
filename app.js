6054ab644c67ea2359231c85de66f642a88345d4
Maxime Favier <maxime1.favier@free.fr>
2018-11-27 22:05:29 +0100

    begin rdv api

--- app.js
+++ app.js
@@ -10,18 +10,20 @@ var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/patientlist');
 var medRouter = require('./routes/medecinlist');
+var rdvRouter = require("./routes/rdvlist");
 var apiPatient = require('./routes/api/patients');
 var apiMedecins = require('./routes/api/medecin');
+var apirdv = require("./routes/api/rdv");
 
 var app = express();
 
 //body parser
 // parse various different custom JSON types as JSON
-app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.json({type: 'application/*+json'}));
 // parse some custom thing into a Buffer
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.raw({type: 'application/vnd.custom-type'}));
 // parse an HTML body into a string
-app.use(bodyParser.text({ type: 'text/html' }));
+app.use(bodyParser.text({type: 'text/html'}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,14 +34,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(bodyParser.json()); // support json encoded bodies
-//app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use('/', indexRouter);
 app.use('/patient', usersRouter);
 app.use('/medecin', medRouter);
+app.use('/rdv', rdvRouter);
 app.use('/api/patient', apiPatient);
 app.use('/api/medecin', apiMedecins);
+app.use('/api/rdv', apirdv);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
